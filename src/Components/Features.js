407d82d5a35c0c0b64b0bd1d3155f01cda36fae1
NisargParmar11 <140941067+NisargParmar11@users.noreply.github.com>
2024-07-13 15:16:20 +0530

    Update Features.js

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -9,7 +9,7 @@ const Features = () => {
         <div className="col-lg-4 col-sm-12 mt-3">
             <div className="card">
                 <div className="image mx-auto" style={{ width:"200px",height:"200px" }}>
-                    <img src="./images/all-natural.png" className="card-img-top w-100 h-100" alt="..."/>
+                    <img src="images/all-natural.png" className="card-img-top w-100 h-100" alt="..."/>
                 </div>
                 <div className="card-body">
                     <h5 className="card-title">Natural Process</h5>
