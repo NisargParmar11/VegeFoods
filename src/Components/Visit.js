e73c21357d6878f2cb97db443cf2ac79807eff89
NisargParmar11 <nis110102@gmail.com>
2024-07-13 14:43:06 +0530

    first commit

---
+++ src/Components/Visit.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Visit = () => {
+  return (
+    <div className="visit container-fluid bg-info mt-5 p-lg-5 p-3">
+            <div className="container">
+                <div className="row mt-4">
+                    <div className="col-lg-8">
+                        <h1>Visit Our Firm</h1>
+                        <p>Come visit our firm and experience our dedication to providing the best in plant-based nutrition and culinary expertise. We're excited to welcome you and share our passion for wholesome, veggie-filled living.</p>
+                    </div>
+                    <div className="col-lg-1"></div>
+                    <div className="col-lg-3 mt-3 d-lg-flex align-items-center">
+                        <button type="button" className="btn btn-primary py-2 px-4">Visit Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Visit
